0c1c7a08c1afa0ebe0941ccf459269b6d8d83aeb
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-03-17 13:40:40 +0000

    changed .env variables

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const cors = require("cors");
 
 // importing veryfi sdk and setting client variables
 const Client = require("@veryfi/veryfi-sdk");
-const client_id = process.env.id;
-const client_secret = process.env.secret;
-const username = process.env.user;
-const api_key = process.env.key;
+const client_id = process.env.ID;
+const client_secret = process.env.SECRET;
+const username = process.env.USER;
+const api_key = process.env.KEY;
 
 let my_client = new Client(client_id, client_secret, username, api_key);
 
 //function that sends the restuarant receipt (invoice.jpg) to veryfi and
 // then returns a json object with all the processed OCR data: used in the below app.get for front end's fetch request.
-const verifiOCR = async () => {
+const verifyOCR = async () => {
   const response = await my_client.process_document("invoice.jpg");
   const data = await response;
   return data;
@@ -26,8 +26,8 @@ app.use(cors());
 
 // for front end to fetch veryfi receipt object
 app.get("/", async (req, res) => {
-  const result = await verifiOCR();
-  res.json({ message: result });
+  const result = await verifyOCR();
+  res.json(result);
 });
 
 app.listen(port, () => {
